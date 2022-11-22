2e4f330631dcc1b9cba29c1062e508a5b297cc14
Sevil20 <sevilmirzayeva010@gmail.com>
2022-11-22 14:40:29 +0400

    bitdi

--- seviltest/src/components/Header/App/Index.jsx
+++ seviltest/src/components/Header/App/Index.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>
+    <div className='container'>
+      <h3>Sevil Mirzeyava</h3>
     <p className='word'>Now i can create component</p>
 
     </div>
