57b047ef8b279ba081c6a00abe75f3865f4474af
Sevil20 <sevilmirzayeva010@gmail.com>
2022-11-22 14:33:45 +0400

    react-layihe

---
+++ seviltest/src/components/Header/App/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+    <p className='word'>Now i can create component</p>
+
+    </div>
+  )
+}
+
+export default index
---
+++ seviltest/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Index from './components/Header/App/Index'
+import App from './components/Header/App/app.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <div>
+   <Index/>
+ </div>
+)
